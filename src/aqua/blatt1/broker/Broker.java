cf69bf9d2370007c37633ef181d65cbfafd5c4de
Ostabo <osi.borkenhagen@gmail.com>
2023-10-16 17:43:31 +0200

    a2 - 1

--- src/aqua/blatt1/broker/Broker.java
+++ src/aqua/blatt1/broker/Broker.java
@@ -9,56 +9,33 @@ import messaging.Message;
 
 import java.io.Serializable;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Broker {
 
+    private static final int THREAD_POOL_SIZE = 10;
     private static final int PORT = 4711;
     private static final Endpoint ENDPOINT = new Endpoint(PORT);
     private static final String ID_PREFIX = "tank";
 
     private final ClientCollection<InetSocketAddress> clients = new ClientCollection<>();
+    private final ReadWriteLock clientLock = new ReentrantReadWriteLock();
+    private volatile boolean done = false;
 
     public static void main(String[] args) {
         new Broker().broker();
     }
 
     private void broker() {
-        while (true) {
-            final Message mes = ENDPOINT.blockingReceive();
-
-            switch (MsgType.valueOf(mes.getPayload())) {
-                case REGISTER -> register(mes.getSender());
-                case DEREGISTER -> deregister(((DeregisterRequest) mes.getPayload()).getId());
-                case HANDOFF -> handoffFish(mes.getSender(), (HandoffRequest) mes.getPayload());
-                case UNKNOWN -> {
-                    System.err.println("Unknown message type: " + mes.getPayload().getClass().getSimpleName());
-                    System.exit(1);
-                }
-            }
+        var executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        while (!done) {
+            final Message msg = ENDPOINT.nonBlockingReceive();
+            if (msg != null)
+                executor.execute(new BrokerTask(msg));
         }
-    }
-
-    private void register(InetSocketAddress client) {
-        final String id = ID_PREFIX + (clients.size() + 1);
-        clients.add(id, client);
-        ENDPOINT.send(client, new RegisterResponse(id));
-    }
-
-    private void deregister(String clientId) {
-        final int index = clients.indexOf(clientId);
-        if (index == -1) {
-            System.err.println("Client not registered...");
-            return;
-        }
-        clients.remove(index);
-    }
-
-    private void handoffFish(InetSocketAddress client, HandoffRequest req) {
-        final int index = clients.indexOf(client);
-        ENDPOINT.send(switch (req.getFish().getDirection()) {
-            case LEFT -> clients.getLeftNeighorOf(index);
-            case RIGHT -> clients.getRightNeighorOf(index);
-        }, req);
+        executor.shutdown();
     }
 
     private enum MsgType {
@@ -72,6 +49,65 @@ public class Broker {
             if (classType instanceof HandoffRequest) return HANDOFF;
             if (classType instanceof RegisterRequest) return REGISTER;
             return UNKNOWN;
+        }
+    }
+
+    public class BrokerTask implements Runnable {
+
+        private final Message msg;
+
+        public BrokerTask(Message msg) {
+            this.msg = msg;
+        }
+
+        @Override
+        public void run() {
+            switch (MsgType.valueOf(msg.getPayload())) {
+                case REGISTER -> register(msg.getSender());
+                case DEREGISTER -> deregister(((DeregisterRequest) msg.getPayload()).getId());
+                case HANDOFF -> handoffFish(msg.getSender(), (HandoffRequest) msg.getPayload());
+                case UNKNOWN -> {
+                    System.err.println("Unknown message type: " + msg.getPayload().getClass().getSimpleName());
+                    done = true;
+                }
+            }
+        }
+
+        private void register(InetSocketAddress client) {
+            clientLock.readLock().lock();
+            final String id = ID_PREFIX + (clients.size() + 1);
+            clientLock.readLock().unlock();
+
+            clientLock.writeLock().lock();
+            clients.add(id, client);
+            clientLock.writeLock().unlock();
+
+            ENDPOINT.send(client, new RegisterResponse(id));
+        }
+
+        private void deregister(String clientId) {
+            clientLock.readLock().lock();
+            final int index = clients.indexOf(clientId);
+            clientLock.readLock().unlock();
+            if (index == -1) {
+                System.err.println("Client not registered...");
+                return;
+            }
+
+            clientLock.writeLock().lock();
+            clients.remove(index);
+            clientLock.writeLock().unlock();
+        }
+
+        private void handoffFish(InetSocketAddress client, HandoffRequest req) {
+            clientLock.readLock().lock();
+            final int index = clients.indexOf(client);
+            clientLock.readLock().unlock();
+
+            ENDPOINT.send(switch (req.getFish().getDirection()) {
+                case LEFT -> clients.getLeftNeighorOf(index);
+                case RIGHT -> clients.getRightNeighorOf(index);
+            }, req);
         }
     }
 
